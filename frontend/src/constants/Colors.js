52d812fe716ec8158936a414d93e0ab204e12038
BasitIqbalatgit <rai.xardar.050@gmail.com>
2024-01-25 21:26:23 +0500

    i have updated the colors.js again

--- frontend/src/constants/Colors.js
+++ frontend/src/constants/Colors.js
@@ -1,5 +1,5 @@
 const primary = '#00ADB5';
-const secondary = '#22CAB8';
+const secondary = '#5A6475';
 const dark = '#161528';
 const white = '#FFFFFF';
 const gradient = 'background: linear-gradient(90deg, #22CAB8 -0.9%, #108A7D 100%)';
@@ -7,11 +7,19 @@ const border = 'border: 1.04px solid #CFCFCF';
 const shadow = 'box-shadow: 0px 8px 10px 0px #00000008';
 
 export default {
+    //button colors
   buttonColor: primary,
-  headingColor: secondary,
-  sidebarColor: dark,
-  textColor: white,
-  navbarColor: gradient,
+  //righ side colors
+  headingColor: secondary,  
   textFieldBorderColor: border,
-  divShadowColor: shadow
+  textColor:secondary,
+  divShadowColor: shadow,
+  
+  //left side bar color:
+  sidebarColor: dark,
+  sideBartextColor: white,
+  lms:"#22CAB8",
+  //nav bar colors
+  navbarTextColor:white,
+  navbarColor: gradient,
 };
