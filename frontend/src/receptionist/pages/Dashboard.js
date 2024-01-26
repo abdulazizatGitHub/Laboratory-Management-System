61a04aad98439768076f5e0cd110378950fe9192
mahadwajid <khandang510@gmail.com>
2024-01-26 17:52:51 +0500

    Update

--- frontend/src/receptionist/pages/Dashboard.js
+++ frontend/src/receptionist/pages/Dashboard.js
@@ -1,57 +1,53 @@
-    // Dashboard.js
-    import React from "react";
-    import '../CSS/Dashboard.css';
+// Dashboard.js
+import React from "react";
+import '../CSS/Dashboard.css';
 
-    const Dashboard = () => {
-        return (
-            <div className="Main-Container">
-         
-                <div className="Profile">
-                    <div className="Image-details">
+const Dashboard = () => {
+    return (
+        <div className="Main-Container">
+            <div className="Profile">
+                <div className="Image-details">
                     <img src=".." className="profile-image" />
+                </div>
 
+                <div className="Profile-details">
+                    <div className="upper-profile">
+                        <p className="bold-light">NAME</p>
+                        <p>Mahad Khan</p>
                     </div>
 
-                    <div className="Profile-details">
-                        <div className="upper-profile">
-                            <span>Name</span> <br></br>
-                            <span>Mahad Khan</span>
-                        </div>
+                    <div className="lower-profile">
+                        <p className="bold-light">FATHER NAME</p>
+                        <p>Khan</p>
+                    </div>
+                </div>
 
-                        <div className="lower-profile">
-                        <span>Father Name</span> <br></br>
-                            <span>Khan</span>
-                        </div>
+                <div className="Middle-details">
+                    <div className="Middle-upper-profile">
+                        <p className="bold-light">GENDER</p>
+                        <p>Male</p>
                     </div>
 
-                    <div className="Middle-details">
-                        <div className="Middle-upper-profile">
-                            <span>Name</span> <br></br>
-                            <span>Hasnin Khan</span>
-                        </div>
+                    <div className="Middle-lower-profile">
+                        <p className="bold-light">DATE OF BIRTH</p>
+                        <p>Khan</p>
+                    </div>
+                </div>
 
-                        <div className="Middle-lower-profile">
-                        <span>Father Name</span> <br></br>
-                            <span>Khan</span>
-                        </div>
+                <div className="Last-details">
+                    <div className="Last-upper-profile">
+                        <p className="bold-light">CONTACT</p>
+                        <p>065415151121</p>
                     </div>
 
-                    <div className="Last-details">
-                        <div className="Last-upper-profile">
-                            <span>Name</span> <br></br>
-                            <span>Mahad Khan</span>
-                        </div>
-
-                        <div className="Last-lower-profile">
-                        <span>Father Name</span> <br></br>
-                            <span>Khan</span>
-                        </div>
+                    <div className="Last-lower-profile">
+                        <p className="bold-light">CNIC</p>
+                        <p>16101-8233468-5</p>
                     </div>
-
-
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
+}
 
-    export default Dashboard;
+export default Dashboard;
