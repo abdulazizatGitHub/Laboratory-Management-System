d4f2f6c713b2046995861a4f2fdc75ab97c6d010
mahadwajid <khandang510@gmail.com>
2024-01-26 14:01:10 +0500

    Update

--- frontend/src/receptionist/components/Sidebar.js
+++ frontend/src/receptionist/components/Sidebar.js
@@ -26,15 +26,15 @@ function Sidebar() {
             <span className='Heading-sidebar'>LMS</span>
 
             
-            <Link to="dashboard"> <p><i className="fa fa-dashboard"></i> Dashboard</p></Link>
+            <Link to="dashboard" style={{textDecoration:'none'}}> <p><i className="fa fa-dashboard"></i> Dashboard</p></Link>
                 
             <button className={`dropdown-btn ${isRegistrationDropdownOpen ? 'active' : ''}`} onClick={handleRegistrationDropdownToggle}>
                 <i className="fa fa-user-plus"></i> Registrations <i className="fa fa-caret-down"></i>
             </button>
             <div className={`dropdown-container ${isRegistrationDropdownOpen ? 'open' : ''}`}>
-            <Link to="patient_registration"> <p>Patient Registration</p></Link>
-            <Link to="search_test"> <p>Search Test</p></Link>
-            <Link to="generate_token"> <p>Generate Token</p></Link>
+            <Link to="patient_registration" style={{textDecoration:'none'}}> <p>Patient Registration</p></Link>
+            <Link to="search_test" style={{textDecoration:'none'}}> <p>Search Test</p></Link>
+            <Link to="generate_token" style={{textDecoration:'none'}}> <p>Generate Token</p></Link>
                 
                 
             </div>
@@ -43,11 +43,11 @@ function Sidebar() {
                 <i className="fa fa-file-text"></i> Test Reports <i className="fa fa-caret-down"></i>
             </button>
             <div className={`dropdown-container ${isTestReportsDropdownOpen ? 'open' : ''}`}>
-                <a href="#">Patient Registration</a>
-                <a href="#">Search Test</a>
+                <p>Patient Registration</p>
+                <p >Search Test</p>
             </div>
 
-            <div className="version">Version 1.0</div>
+            {/* <div className="version">Version 1.0</div> */}
             
         </div>
         </div>
