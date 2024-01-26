7d231d2110d56f70f228c7dc44ca086d7b1ca9fb
mahadwajid <khandang510@gmail.com>
2024-01-26 09:29:47 +0500

    Update on Dashbord

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from './receptionist/components/Sidebar';
 import Navbar from './receptionist/components/Navbar';
+import Dashboard from './receptionist/pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
+      {/* <Sidebar /> */}
       {/* <Navbar /> */}
+      <Dashboard />
     </div>
   );
 }
--- frontend/src/receptionist/pages/Dashboard.js
+++ frontend/src/receptionist/pages/Dashboard.js
@@ -1,10 +1,32 @@
-import React from "react"
+    // Dashboard.js
+    import React from "react";
+    import Sidebar from "../components/Sidebar";
+    import '../CSS/Dashboard.css';
 
+    const Dashboard = () => {
+        return (
+            <div>
+                <Sidebar />
+                <div className="Profile">
+                    <div className="Image-details">
+                    <img src=".." className="profile-image" />
 
-const Dashboard = ()=>{
-    return(<div>
+                    </div>
 
-    </div>)
-}
+                    <div className="Profile-details">
+                        <div className="upper-profile">
+                            <span>Name</span> <br></br>
+                            <span>Mahad Khan</span>
+                        </div>
 
-export default Dashboard;
+                        <div className="lower-profile">
+                        <span>Father Name</span> <br></br>
+                            <span>Khan</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    export default Dashboard;
