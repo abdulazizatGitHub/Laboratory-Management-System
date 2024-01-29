a2a6fa318e9219beb3595149ae0528c44980d309
Abdul Aziz <129079186+abdulazizatGitHub@users.noreply.github.com>
2024-01-29 05:21:07 +0500

    i have make changes in the registration page

--- frontend/src/receptionist/pages/PatientRegistration.js
+++ frontend/src/receptionist/pages/PatientRegistration.js
@@ -17,6 +17,12 @@ const PatientRegistration = () => {
     patientRemarks: '',
   });
 
+  const [selectedGender, setSelectedGender] = useState('');
+
+  const handleGenderChange = (event) => {
+    setSelectedGender(event.target.value);
+  };
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -25,11 +31,11 @@ const PatientRegistration = () => {
     }));
   };
 
-  
+
   const handleNextClick = async () => {
     try {
       const registrationSuccess = await registerPatient(formData);
-  
+
       if (registrationSuccess) {
         // Display an alert with the success message
         window.alert('Patient registered successfully');
@@ -43,70 +49,81 @@ const PatientRegistration = () => {
       console.error('Error during registration:', error);
     }
   };
-  
+
 
 
   return (
 
-    <div className="Main">
-
-      <section className="Profile-Main-Container">
-
-        <div className="Patient-Profile">
+    <div className="Patient-Main-Container">
+      <div className="Patient-Details-Container">
           <h2>Patient Details</h2>
-          <div className="Patient-Personal-Profile">
+          <div className="Personal-Details">
             <div className="left-side-Profile">
               <label for="fname">Patient name</label><br></br>
-              <input type="text" id="name" name="name" placeholder="Enter Your Name" value={formData.name} onChange={handleInputChange} /><br></br>
+              <input type="text" id="name" name="name" placeholder="Enter Patient Name" value={formData.name} onChange={handleInputChange} /><br></br>
 
               <label for="fname">Gender</label><br></br>
-              <input type="text" id="gender" name="gender" placeholder="Enter Your Gender" value={formData.gender} onChange={handleInputChange} />
+              <div className="Patient-Gender-Container">
+                <label>
+                  <input
+                    type="radio"
+                    value="male"
+                    checked={selectedGender === 'male'}
+                    onChange={handleGenderChange}
+                  />
+                  Male
+                </label>
+
+                <label>
+                  <input
+                    type="radio"
+                    value="female"
+                    checked={selectedGender === 'female'}
+                    onChange={handleGenderChange}
+                  />
+                  Female
+                </label>
+              </div>
             </div>
 
             <div className="Right-side-Profile">
               <label for="fname">Age</label><br></br>
-              <input type="Number" id="age" name="age" placeholder="Enter Your Name" value={formData.age} onChange={handleInputChange} /><br></br>
+              <input type="Number" id="age" name="age" placeholder="Enter Patient Age" value={formData.age} onChange={handleInputChange} /><br></br>
 
               <label for="fname">CNIC</label><br></br>
-              <input type="text" id="cnic" name="cnic" placeholder="Enter Your Gender" value={formData.cnic} onChange={handleInputChange} />
+              <input type="text" id="cnic" name="cnic" placeholder="Enter Patient CNIC" value={formData.cnic} onChange={handleInputChange} />
             </div>
           </div>
 
           <h2>Patient Contact Details</h2>
-          <div className="Patient-contact">
+          <div className="Contact-Details">
             <div className="left-side-Profile">
               <label for="fname">Mobile Number</label><br></br>
-              <input type="text" id="mobileNumber" name="mobileNumber" placeholder="Enter Your Contact" value={formData.mobileNumber}
+              <input type="text" id="mobileNumber" name="mobileNumber" placeholder="Enter Patient Contact" value={formData.mobileNumber}
                 onChange={handleInputChange} /><br></br>
 
               <label for="fname">Address</label><br></br>
-              <input type="text" id="address" name="address" placeholder="Enter Your Address" value={formData.address}
+              <input type="text" id="address" name="address" placeholder="Enter Patient Address" value={formData.address}
                 onChange={handleInputChange} />
-
             </div>
 
             <div className="Right-side-Profile">
               <label for="fname">Email</label><br></br>
-              <input type="text" id="email" name="email" placeholder="Enter Your Emal" value={formData.email}
+              <input type="text" id="email" name="email" placeholder="Enter Patient Email" value={formData.email}
                 onChange={handleInputChange} /><br></br>
-
-
-
             </div>
-
-
           </div>
 
-          <h2>Contact Details</h2>
-          <div className="Doctor-Contact">
+          <h2>Visit Details</h2>
+          <div className="Visit-Details">
             <div className="left-side-Profile">
-              <label for="fname">Reff. Doctor</label><br></br>
+              <label for="fname">Reffered By</label><br></br>
               <input type="text" id="refDoctor" name="refDoctor" placeholder="Enter Your Name" value={formData.refDoctor}
                 onChange={handleInputChange} /><br></br>
 
-              <label for="fname">Internal Remarks</label><br></br>
+              {/* <label for="fname">Internal Remarks</label><br></br>
               <input type="text" id="internalRemarks" name="internalRemarks" placeholder="Enter Your Rmarks" value={formData.internalRemarks}
-                onChange={handleInputChange} />
+                onChange={handleInputChange} /> */}
 
             </div>
 
@@ -114,22 +131,15 @@ const PatientRegistration = () => {
               <label for="fname">Patient Remarks</label><br></br>
               <input type="text" id="patientRemarks" name="patientRemarks" placeholder="Enter Patient Remarks" value={formData.patientRemarks}
                 onChange={handleInputChange} /><br></br>
-
-
-
             </div>
-
-
           </div>
-
-          <button className="Next-button" onClick={handleNextClick}>Next</button>
+          <div className="btn-container"><button className="Next-button" onClick={handleNextClick}>Next</button></div>
         </div>
 
-        <div className="Patient-Image">
+      {/* <section className="Profile-Main-Container">
 
-        </div>
-
-      </section>
+        
+      </section> */}
 
     </div>
   )
