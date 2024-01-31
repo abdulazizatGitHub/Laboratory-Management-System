5e06f19c0eb652624c5554f14d8547707e58f823
mahadwajid <khandang510@gmail.com>
2024-01-31 19:31:27 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import Sidebar from './receptionist/components/Sidebar';
-import Navbar from './receptionist/components/Navbar';
-import Dashboard from './receptionist/pages/Dashboard';
 import Main from "./receptionist/pages/Main";
 import { Route, Routes } from 'react-router';
 import Login from './receptionist/pages/Login';
