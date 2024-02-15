43aa5a16d4b80fbd559d80748867375d9b6aafc4
BasitIqbalatgit <rai.xardar.050@gmail.com>
2024-02-15 10:43:52 +0500

    BASIt

--- frontend/src/phelobotony/component/Sidebar.js
+++ frontend/src/phelobotony/component/Sidebar.js
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 
 function Sidebar() {
 
+    const [isMobileScreen, setIsMobileScreen] = useState(false);
 
     return (
         <div className="sidebar-container">
