8871562daa0cf46b9ff8ce5cdff346e98d429f98
mahadwajid <khandang510@gmail.com>
2024-02-18 16:16:32 +0500

    Upadte

--- frontend/src/Services/API.js
+++ frontend/src/Services/API.js
@@ -30,10 +30,10 @@ export const addTest = async (test) =>{
   return await axios.post(`${Url}/admin/Addtest`,test);
 }
 
-// Update token count in the backend API
-export const updateTokenCount = async () => {
-  return await axios.put(`${Url}/receptionist/generate_token`);
-};
+// // Update token count in the backend API
+// export const updateTokenCount = async () => {
+//   return await axios.put(`${Url}/receptionist/generate_token`);
+// };
 
 //Saved the TOKEN DATA 
 export const saveToken = async (tokenData) =>{
@@ -46,16 +46,16 @@ export const fetchtests = async () =>{
   return await axios.get(`${Url}/admin/Addtest`);
 }
 
-// Fetch token count from the backend API
-export const fetchTokenCount = async () => {
-    try {
-      const response = await axios.get(`${Url}/receptionist/generate_token`); // Adjust the endpoint according to your backend setup
-      return response.data.tokenCount;
-    } catch (error) {
-      console.error('Error fetching token count:', error);
-      throw error; // Propagate the error to the calling function/component
-    }
-  };
+// // Fetch token count from the backend API
+// export const fetchTokenCount = async () => {
+//     try {
+//       const response = await axios.get(`${Url}/receptionist/generate_token`); // Adjust the endpoint according to your backend setup
+//       return response.data.tokenCount;
+//     } catch (error) {
+//       console.error('Error fetching token count:', error);
+//       throw error; // Propagate the error to the calling function/component
+//     }
+//   };
 
 //Get No of Test Counts ///////////
 export const getAllTests = async () =>{
