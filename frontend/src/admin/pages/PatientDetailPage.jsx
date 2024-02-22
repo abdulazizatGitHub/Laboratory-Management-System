a672629b49585ed42d91bc4b8d6d45c660a5d3ed
BasitIqbalatgit <rai.xardar.050@gmail.com>
2024-02-22 20:48:05 +0500

    Basit

--- frontend/src/admin/pages/PatientDetailPage.jsx
+++ frontend/src/admin/pages/PatientDetailPage.jsx
@@ -1,104 +1,84 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "../css/PatientDetailPage.css";
 import { useLocation } from "react-router-dom";
 const PatientDetailPage = () => {
-    const location= useLocation();
-    const {data} = location.state;
-    useEffect(()=>{
-        console.log("contact No: ",data);
-    },[]);
+    const location = useLocation();
+    const { data } = location.state;
+    useEffect(() => {
+        console.log("contact No: ", data);
+    }, []);
     return (<div id="patientDetailPage-Fullcontainer">
         <div id="pdp-nameAndButtons">
             <p id="pdp-name">Patient Detail</p>
-           
-                <button type="button" id="pdp-btn">Edit</button>
-                <button type="button" id="pdp-btn">Delete</button>
-            
+            <button type="button" id="pdp-btn">Edit</button>
+            <button type="button" id="pdp-btn">Delete</button>
         </div>
         <div id="pdp-container">
             <p className="pdp-heading">Personal Detail</p>
             <div className="pdp-details">
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading">Name: </p>
-                        <p>{data.name}</p>
-                    </div>.
+                <div className="pdp-subBoxes">
+                    <p className="pdp-smallHeading">Name: </p>
+                    <p>{data.name}</p>
+                </div>.
 
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading">Father Name: </p>
-                        <p>Abdul AZiz</p>
-                    </div>.
+                <div className="pdp-subBoxes">
+                    <p className="pdp-smallHeading">Father Name: </p>
+                    <p>Abdul AZiz</p>
+                </div>.
 
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading">Age: </p>
-                        <p>22</p>
-                    </div>.
+                <div className="pdp-subBoxes">
+                    <p className="pdp-smallHeading">Age: </p>
+                    <p>22</p>
+                </div>.
 
-                    
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading">Gender: </p>
-                        <p>Male</p>
-                    </div>
+                <div className="pdp-subBoxes">
+                    <p className="pdp-smallHeading">Gender: </p>
+                    <p>Male</p>
+                </div>.
 
-                    
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading">CNIC: </p>
-                        <p>{data.cnic}</p>
-                    </div>.
+
+                <div className="pdp-subBoxes">
+                    <p className="pdp-smallHeading">CNIC: </p>
+                    <p>{data.cnic}</p>
+                </div>.
             </div>
 
-            {/* <p className="pdp-heading"></p>
-            <div className="pdp-details">
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading"> </p>
-                        <p></p>
-                    </div>
-
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading"> </p>
-                        <p></p>
-                    </div>.
-
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading"> </p>
-                        <p></p>
-                    </div>                 
-            </div> */}
 
             <p className="pdp-heading">Contact Detail</p>
             <div className="pdp-details">
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading">Contact#: </p>
-                        <p>{data.mobileNumber}</p>
-                    </div>.
+                <div className="pdp-subBoxes">
+                    <p className="pdp-smallHeading">Contact#: </p>
+                    <p>{data.mobileNumber}</p>
+                </div>.
 
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading">Email: </p>
-                        <p>ABC@gmail.com</p>
-                    </div>.
+                <div className="pdp-subBoxes">
+                    <p className="pdp-smallHeading">Email: </p>
+                    <p>ABC@gmail.com</p>
+                </div>.
 
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading">Address: </p>
-                        <p>Abbottabad</p>
-                    </div>
+                <div className="pdp-subBoxes">
+                    <p className="pdp-smallHeading">Address: </p>
+                    <p>Abbottabad</p>
                 </div>
+            </div>
 
             <p className="pdp-heading">Visiting Detail</p>
             <div className="pdp-details">
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading">Registration no: </p>
-                        <p>01/01/2024, 1:45 AM</p>
-                    </div>.
+                <div className="pdp-subBoxes">
+                    <p className="pdp-smallHeading">Registration no: </p>
+                    <p>01/01/2024, 1:45 AM</p>
+                </div>.
 
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading">Last visit on: </p>
-                        <p>20/01/2024, 12:45 PM</p>
-                    </div>.
+                <div className="pdp-subBoxes">
+                    <p className="pdp-smallHeading">Last visit on: </p>
+                    <p>20/01/2024, 12:45 PM</p>
+                </div>.
 
-                    <div className="pdp-subBoxes">
-                        <p className="pdp-smallHeading">No of Visit: </p>
-                        <p>04</p>
-                    </div>
+                <div className="pdp-subBoxes">
+                    <p className="pdp-smallHeading">No of Visit: </p>
+                    <p>04</p>
                 </div>
+            </div>
 
 
         </div>
