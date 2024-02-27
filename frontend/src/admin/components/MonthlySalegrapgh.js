1b31d3b0b4f924f7170e8873dfe73645936b1ab3
mahadwajid <khandang510@gmail.com>
2024-02-27 22:44:19 +0500

    Update

--- frontend/src/admin/components/MonthlySalegrapgh.js
+++ frontend/src/admin/components/MonthlySalegrapgh.js
@@ -1,48 +1,51 @@
 import React, { useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
 
-const MonthlySalesChart = ({ data }) => {
-  const chartRef = useRef(null);
+const MonthlySalesChart = ({ monthly }) => {
+    const chartRef = useRef(null);
 
-  useEffect(() => {
-    if (chartRef.current) {
-      // If a chart instance already exists, destroy it before creating a new one
-      chartRef.current.destroy();
-    }
-
-    const ctx = document.getElementById('monthlySalesChart');
-    const newChartInstance = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: data.map(item => item.dateOfMonth),
-        datasets: [{
-          label: 'Monthly Sales',
-          data: data.map(item => item.sales),
-          borderColor: 'rgb(75, 192, 192)',
-          tension: 0.1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
-
-    // Save the reference to the chart instance
-    chartRef.current = newChartInstance;
-
-    // Cleanup function to destroy the chart instance when component unmounts
-    return () => {
+    useEffect(() => {
       if (chartRef.current) {
-        chartRef.current.destroy();
+          // If a chart instance already exists, destroy it before creating a new one
+          chartRef.current.destroy();
       }
-    };
-  }, [data]);
+   
+      console.log("the monthly data", monthly);
+    
+  
+      const ctx = document.getElementById('monthlySalesChart');
+      const newChartInstance = new Chart(ctx, {
+          type: 'line',
+          data: {
+              labels: monthly.map(item => item.dateOfMonth),
+              datasets: [{
+                  label: 'Monthly Sales',
+                  data: monthly.map(item => item.sales),
+                  borderColor: 'rgb(75, 192, 192)',
+                  tension: 0.1
+              }]
+          },
+          options: {
+              scales: {
+                  y: {
+                      beginAtZero: true
+                  }
+              }
+          }
+      });
+  
+      // Save the reference to the chart instance
+      chartRef.current = newChartInstance;
+  
+      // Cleanup function to destroy the chart instance when component unmounts
+      return () => {
+          if (chartRef.current) {
+              chartRef.current.destroy();
+          }
+      };
+  }, [monthly]);
 
-  return <canvas id="monthlySalesChart" width="400" height="200"></canvas>;
+    return <canvas id="monthlySalesChart" width="400" height="200"></canvas>;
 };
 
 export default MonthlySalesChart;
